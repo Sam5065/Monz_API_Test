e71a469da04028ce102e14db73f218157f78859c
Sam H <sam.hilland1@icloud.com>
2024-11-17 13:25:44 +0000

    feat: initial commit with sample Dart application structure and basic functionality

---
+++ bin/mozo_dart_api.dart
@@ -0,0 +1,5 @@
+import 'package:mozo_dart_api/mozo_dart_api.dart' as mozo_dart_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mozo_dart_api.calculate()}!');
+}
---
+++ test/mozo_dart_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:mozo_dart_api/mozo_dart_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
